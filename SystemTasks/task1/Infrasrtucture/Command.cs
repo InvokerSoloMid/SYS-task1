34ab7ff3a958fa48033617830ebd15e6d6b14968
macbook <macbook@MacBook-Pro-macbook.local>
2020-02-24 23:32:49 +0200

    add program

---
+++ SystemTasks/task1/Infrasrtucture/Command.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace task1.Infrasrtucture
+{
+    public class Command : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+        private event EventHandler CanExecuteChangedInternal;
+
+        public Command(Action<object> execute) : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public Command(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException("execute");
+            this.canExecute = canExecute ?? throw new ArgumentNullException("canExecute");
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                CanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                CanExecuteChangedInternal -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute != null && canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChangedInternal;
+            if (handler != null)
+            {
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Destroy()
+        {
+            canExecute = _ => false;
+            execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
---
+++ SystemTasks/task1/ViewModels/MainViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using task1.Infrasrtucture;
+
+namespace task1.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string fileLocation;
+        private string text;
+        private Encoding currenEncoding;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+
+        public Encoding CurrentEncoding
+        {
+            get
+            {
+                return currenEncoding;
+            }
+            set
+            {
+                currenEncoding = value;
+                OnPropertyChanged(nameof(CurrentEncoding));
+                OnPropertyChanged(nameof(IsAscii));
+                OnPropertyChanged(nameof(IsUnicode));
+            }
+        }
+
+        public bool IsAscii => CurrentEncoding == Encoding.ASCII;
+        public bool IsUnicode => !IsAscii;
+
+        public ICommand OpenFileCommand { get; private set; }
+        public ICommand SaveFileCommand { get; private set; }
+        public ICommand ChangeCodingCommnad { get; private set; }
+
+        public MainViewModel()
+        {
+            CurrentEncoding = Encoding.ASCII;
+            OpenFileCommand = new Command(OpenFile);
+            SaveFileCommand = new Command(SaveFile);
+            ChangeCodingCommnad = new Command(ChangeCoding);
+        }
+
+        private async void OpenFile(object parameter)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "TXT(*.TXT)|*.txt";
+            if (ofd.ShowDialog() ?? true)
+            {
+                fileLocation = ofd.FileName;
+                using (var fileStr = new StreamReader(fileLocation, CurrentEncoding))
+                {
+                    Text = await fileStr.ReadToEndAsync();
+                }
+            }
+        }
+
+        private async void SaveFile(object parameter)
+        {
+            using (var fileStr = new StreamWriter(File.Open(fileLocation, FileMode.Open), CurrentEncoding))
+            {
+                await fileStr.WriteAsync(Text);
+            }
+        }
+
+        private async void ChangeCoding(object parameter)
+        {
+            string type = (string)parameter;
+            switch (type)
+            {
+                case "Ascii":
+                    {
+                        CurrentEncoding = Encoding.ASCII;
+                        if (!string.IsNullOrEmpty(fileLocation))
+                        {
+                            using (var fileStr = new StreamReader(fileLocation, CurrentEncoding))
+                            {
+                                Text = await fileStr.ReadToEndAsync();
+                            }
+                        }
+
+                        break;
+                    }
+                case "Unicode":
+                    {
+                        CurrentEncoding = new UTF8Encoding(false);
+                        if (!string.IsNullOrEmpty(fileLocation))
+                        {
+                            using (var fileStr = new StreamReader(fileLocation, CurrentEncoding))
+                            {
+                                Text = await fileStr.ReadToEndAsync();
+                            }
+                        }
+
+                        break;
+                    }
+                default:
+                    ChangeCoding("Unicode");
+                    break;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
